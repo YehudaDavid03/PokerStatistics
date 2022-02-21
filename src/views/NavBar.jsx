0dfebdd6575e2ff1ba5e5547f8074424f0402ee2
Yehuda Mordechai David <yehudadavid@Yehudas-MacBook-Pro.local>
2022-02-21 07:01:56 +0200

    Mobile device fourth try

--- src/views/NavBar.jsx
+++ src/views/NavBar.jsx
@@ -8,7 +8,7 @@ const NavBar = ({ gamesRender, loading, userInfoReceived, logout }) => {
   }
   return (
     <div className="nav-bar-main">
-      {userInfoReceived.jwt ? <h5 onClick={() => {logout()}}>Logout <span className="material-icons-outlined">arrow_forward_ios</span></h5> : <h1>Poker Statistic DashBoard <span className="material-icons-outlined">arrow_forward_ios</span></h1>}
+      {userInfoReceived.jwt ? <h4 onClick={() => {logout()}}><span className="material-icons-outlined">logout</span> Logout</h4> : <h1>Poker Statistic DashBoard</h1>}
       {userInfoReceived.jwt ? <h4><span className="material-icons-outlined">military_tech</span>{`Total Bets ${loading ? "0" : r.toLocaleString()}nis`}</h4> : <></>}
     </div>
   )
